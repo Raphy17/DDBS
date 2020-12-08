802373a359f501e19b38b1cdf0b50887105d095b
Ülkü Karagöz <uelkue.karagoez@uzh.ch>
2020-12-08 19:19:20 +0100

    csvvv

--- Tests.py
+++ Tests.py
@@ -6,7 +6,7 @@ import csv
 #workers
 
 
-for input_size in range(1000, 20000, 200):
+for input_size in range(1000, 1500, 200):
     band_condition = [2, 2, 2]      #band join predicate
     nr_w = 5                        #number of workers
     sample_size = input_size//20               #sample size (best to choose something divisible by nr_w)
@@ -16,27 +16,25 @@ for input_size in range(1000, 20000, 200):
     all_statistics.append(statistics)
 
 
-def write_csv_file(file, all_statistics):
-    with open(file, "r+") as f:
-        writer = csv.writer(file)
-        writer.writerow(["Duration rec part", "Total time distribution", "Slowest time single distribution", "Total time join",
-                  "Slowest time single join", "Total time", "Percentage of time spent in recPart",
-                  "Sample input before duplication", "Total sample input after duplication", "Estimated Input overhead",
-                  "Real Input before duplication", "Estimated Input after duplication", "Real Input after duplication",
-                  "Real Input overhead", "Output size",
-                  "lower bound workload per machine", "workload of worst machine", "Estimated workload overhead",
-                  "real lower bound workload per machine", "real workload of worst machine", "real workload overhead"
-                  ])
+def write_csv_file(file, results):
+    with open(file, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Duration rec part"], ["Total time distribution"], ["Slowest time single distribution"],
+                        ["Total time join"], ["Slowest time single join"], ["Total time"],
+                        ["Percentage of time spent in recPart"],
+                        ["Sample input before duplication"], ["Total sample input after duplication"],
+                        ["Estimated Input overhead"], ["Real Input before duplication"], ["Estimated Input after duplication"],
+                        ["Real Input after duplication"], ["Real Input overhead"], ["Output size"],
+                        ["lower bound workload per machine"], ["workload of worst machine"], ["Estimated workload overhead"],
+                        ["real lower bound workload per machine"], ["real workload of worst machine"], ["real workload overhead"]
+                  )
+
+        for stat in results:
+            a, b, c = stat
+
+            writer.writerow([a[0]], [a[1]], [a[2]], [a[3]], [a[4]], [a[5]], [a[6]],
+                             [b[0]], [b[1]], [b[2]], [b[3]], [b[4]], [b[5]], [b[6]], [b[7]],
+                             [c[0]], [c[1]], [c[2]], [c[3]], [c[4]], [c[5]])
 
 
-
-        for statistics in all_statistics:
-            a, b, c = statistics
-            range = len(a)+len(b)+len(c)
-
-            for i in range(range):
-                writer.writerow([a[0], a[1], a[2], a[3], a[4], a[5], a[6],
-                                 b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7],
-                                 c[0], c[1], c[2], c[3], c[4], c[5]])
-
-
+write_csv_file("Results.csv", all_statistics)
